617e8721983548896668cbae5f8f453885af41e9
Bryce Carter <bryce.carte@gmail.com>
2016-09-21 19:13:04 -0400

    Update main_dust.py

--- main_dust.py
+++ main_dust.py
@@ -4,20 +4,18 @@ from margolus import *
 
 def main():
     SIZE = 130
-    GRID = initialize_grid1(SIZE)
+    GRID = initialize_grid_normal(SIZE)
     PARITY = 0
-    INCR = 0
 
     fig = plt.figure()
     img_plot = plt.imshow(GRID, interpolation="nearest", cmap=plt.cm.gray)
     plt.show(block=False)
 
-    while INCR < 2000:
+    while True:
         margolus_gen(GRID, DUST_RULES, PARITY, boundary="H_WRAP")
         img_plot.set_data(GRID)
         fig.canvas.draw()
         PARITY = 1 - PARITY
-        INCR += 1
 
 if __name__ == "__main__":
     main()
