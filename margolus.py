e90a92fbe71e3138487785581e7df36da53592c1
Bryce Carter <bryce.carte@gmail.com>
2016-09-21 15:19:18 -0400

    Update margolus.py

--- margolus.py
+++ margolus.py
@@ -1,57 +1,18 @@
-class RangeMap(dict):
-
-    def __init__(self, iterable=None):
-        if isinstance(iterable, dict):
-            for k in iterable:
-                self[k] = iterable[k]
-        elif isinstance(iterable, (list, tuple)):
-            for k, v in enumerate(iterable):
-                self[k] = v
-
-    def __getitem__(self, key):
-        return self.get(key, key)
-
-
 class MargolusRules(dict):
 
     KEY_MATCH = {
-        0:  (0, 0, 0, 0),
-        1:  (1, 0, 0, 0),
-        2:  (0, 1, 0, 0),
-        3:  (1, 1, 0, 0),
-        4:  (0, 0, 1, 0),
-        5:  (1, 0, 1, 0),
-        6:  (0, 1, 1, 0),
-        7:  (1, 1, 1, 0),
-        8:  (0, 0, 0, 1),
-        9:  (1, 0, 0, 1),
-        10: (0, 1, 0, 1),
-        11: (1, 1, 0, 1),
-        12: (0, 0, 1, 1),
-        13: (1, 0, 1, 1),
-        14: (0, 1, 1, 1),
-        15: (1, 1, 1, 1)
+        0:  (0, 0, 0, 0), 1:  (1, 0, 0, 0), 2:  (0, 1, 0, 0), 3:  (1, 1, 0, 0),
+        4:  (0, 0, 1, 0), 5:  (1, 0, 1, 0), 6:  (0, 1, 1, 0), 7:  (1, 1, 1, 0),
+        8:  (0, 0, 0, 1), 9:  (1, 0, 0, 1), 10: (0, 1, 0, 1), 11: (1, 1, 0, 1),
+        12: (0, 0, 1, 1), 13: (1, 0, 1, 1), 14: (0, 1, 1, 1), 15: (1, 1, 1, 1)
     }
 
     def __init__(self, iterable=None):
         if isinstance(iterable, (list, tuple)):
-            for k in RangeMap(iterable):
-                self[MargolusRules.KEY_MATCH[k]] = MargolusRules.KEY_MATCH[iterable[k]]
-
-    def __getitem__(self, k):
-        return self.get(k, k)
-
-    def set_swap(self, n0, n1):
-        self[n0] = n1
-        self[n1] = n0
-
-    def set_cycle(self, ns):
-        for n0, n1 in zip(ns, ns[1:] + [ns[0]]):
-            self[n0] = n1
-
-    def set_eqs(self, ns, n1):
-        for n0 in ns:
-            self[n0] = n1
+            for i, n in enumerate(iterable):
+                k = MargolusRules.KEY_MATCH[i]
+                v = MargolusRules.KEY_MATCH[n]
+                self[k] = v
 
 
 def update_neighborhood(i, j, grid, rules):
@@ -61,7 +22,7 @@ def update_neighborhood(i, j, grid, rules):
         (i % m, (j+1) % n),
         ((i+1) % m, j % n),
         ((i+1) % m, (j+1) % n)]
-    old_block = tuple(grid[ij] for ij in neighborhood)
+    old_block = tuple(map(lambda ij: grid[ij], neighborhood))
     new_block = rules[old_block]
     for ij, val in zip(neighborhood, new_block):
         grid[ij] = val
