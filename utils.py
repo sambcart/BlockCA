8c1e050d237d164d1ef105bf0ad60c821baac977
Bryce Carter <bryce.carte@gmail.com>
2016-09-21 19:19:46 -0400

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,7 +1,6 @@
 import numpy as np
 from margolus import MargolusRules
 
-
 TRON_RULES     = MargolusRules((15, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 0))
 HPP_RULES      = MargolusRules((0, 8, 4, 12, 2, 10, 9, 14, 1, 6, 5, 13, 3, 11, 7, 15))
 BBM_RULES      = MargolusRules((0, 8, 4, 3, 2, 5, 9, 7, 1, 6, 10, 11, 12, 13, 14, 15))
@@ -17,7 +16,6 @@ STRING2_RULES  = MargolusRules((0, 1, 2, 12, 4, 10, 6, 7, 8, 9, 5, 11, 3, 13, 14
 DIAGSWAP_RULES = MargolusRules((0, 8, 4, 12, 2, 10, 6, 14, 1, 9, 5, 13, 3, 11, 7, 15))
 DUST_RULES     = MargolusRules((0, 4, 8, 12, 4, 12, 12, 13, 8, 12, 12, 14, 12, 13, 14, 15))
 
-
 def initialize_grid_sparse_random(size):
     grid = np.zeros((size, size), dtype=np.uint8)
     num = (size*size / 400) + 1
@@ -29,21 +27,21 @@ def initialize_grid_sparse_random(size):
 def initialize_grid_random(size):
     return np.random.randint(2, size=(size, size), dtype=np.uint8)
 
-def initialize_grid0(size):
-    grid = np.zeros((size, size), dtype=np.uint8)
-    grid[size/3, size/3] = 1
-    grid[size/3, 2*size/3] = 1
-    grid[2*size/3, size/3] = 1
-    grid[2*size/3, 2*size/3] = 1
-    return grid
-
-def initialize_grid1(size):
+def initialize_grid_normal(size):
     grid = np.zeros((size, size), dtype=np.uint8)
     num = (size*size / 10) + 1
     for _ in xrange(num):
         i = np.random.normal(size/2, size/15)
         j = np.random.normal(size/2, size/15)
         grid[int(i), int(j)] = 1
+    return grid
+
+def initialize_grid_points(size):
+    grid = np.zeros((size, size), dtype=np.uint8)
+    grid[size/3, size/3] = 1
+    grid[size/3, 2*size/3] = 1
+    grid[2*size/3, size/3] = 1
+    grid[2*size/3, 2*size/3] = 1
     return grid
 
 def initialize_grid_lines(size):
